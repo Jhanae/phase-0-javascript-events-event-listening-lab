c81e211171545f0c480e7ad800a826dfc6ebde51
Jhanae <jhanaechisholm12@gmail.com>
2021-05-08 12:22:33 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function clickalert(){
+    alert('I was clicked!');
 }
+function addingEventListener() {
+    const input = document.getElementById('input');
+
+    input.addEventListener('click', clickalert); 
+}
+addingEventListener();
